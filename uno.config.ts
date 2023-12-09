f5cd4e507704026adac8155666c688c5cd84a82a
Patrick Raedler <code@readpato.dev>
2023-12-09 19:20:07 +0100

    chore(uno.config): add font and transformer directives

--- uno.config.ts
+++ uno.config.ts
@@ -1,13 +1,23 @@
 import { defineConfig, presetAttributify, presetUno, presetWebFonts } from 'unocss'
+import transformerDirectives from '@unocss/transformer-directives'
 
 export default defineConfig({
   presets: [
     presetUno(),
     presetAttributify(),
-    presetWebFonts(),
+    presetWebFonts({
+      provider: 'google',
+      fonts: {
+        sans: 'Roboto',
+        mono: 'Fira Code',
+      },
+    }),
   ],
   shortcuts: {
     'bg-primary': 'bg-[#1a1a1a]',
     'border-base': 'border-gray:40',
   },
+  transformers: [
+    transformerDirectives(),
+  ],
 })
