a630bf8207d27d54eb9f1fa4a68c9fbfbf5fb200
Patrick Raedler <code@readpato.dev>
2023-12-09 14:16:39 +0100

    feat(TheNav): create component

---
+++ components/TheNav.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import SHLogo from '@/assets/svg/sh-logo.svg'
+
+const REPO_URL = 'https://github.com/Schrodinger-Hat/sh-website-cms'
+const ORG_URL = 'https://github.com/Schrodinger-Hat/'
+</script>
+
+<template>
+  <div flex="~ justify-between items-center" p-3 border-b border-base>
+    <NuxtLink :to="ORG_URL" target="_blank">
+      <SHLogo w-8 h-8 />
+    </NuxtLink>
+    <NuxtLink :to="REPO_URL" target="_blank">
+      <Icon w-6 h-6 name="carbon:logo-github" />
+    </NuxtLink>
+  </div>
+</template>
