d5b7dc11492353e7de3fbf0264785456dfad4c0f
Patrick Raedler <code@readpato.dev>
2023-12-09 18:03:55 +0100

    refactor(app): modify templating, add optimizing scss

--- app.vue
+++ app.vue
@@ -1,3 +1,20 @@
 <template>
-  <TheLayout />
+  <TheLayout>
+    <div bg-primary h-200dvh />
+  </TheLayout>
 </template>
+
+<style lang="scss">
+body {
+  color: white;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  font-variant-ligatures: none;
+  letter-spacing: 0;
+  line-height: 1;
+  overscroll-behavior: none;
+  -webkit-tap-highlight-color: transparent;
+  text-rendering: optimizelegibility;
+  text-size-adjust: none;
+}
+</style>
