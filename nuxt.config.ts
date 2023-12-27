6ac04aef95a235bcac4b892da112d7522004bac7
Patrick Raedler <code@readpato.dev>
2023-12-27 15:36:26 +0100

    chore(deps): update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ const sassAdditionalData = () => {
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@unocss/nuxt', '@vueuse/nuxt', 'nuxt-vitest', 'nuxt-svgo', 'nuxt-icon'],
+  modules: ['@unocss/nuxt', '@vueuse/nuxt', '@nuxt/test-utils/module', 'nuxt-svgo', 'nuxt-icon'],
   css: ['@unocss/reset/tailwind.css'],
   svgo: {
     defaultImport: 'component',
