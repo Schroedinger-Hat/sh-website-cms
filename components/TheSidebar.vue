d4bc6143e3b431962c6c4a136c99bf6814270041
Patrick Raedler <code@readpato.dev>
2024-02-11 15:40:38 +0100

    refactor(TheSidebar): add css for handling drawer

--- components/TheSidebar.vue
+++ components/TheSidebar.vue
@@ -1,4 +1,8 @@
 <script setup lang="ts">
+defineProps<{
+  show: boolean
+}>()
+
 const links = [
   {
     text: 'Code of Conduct',
@@ -29,19 +33,18 @@ const links = [
 </script>
 
 <template>
-  <aside class="aside" h-100dvh p3 fixed left-0 border-r border-base bg-inherit>
+  <aside
+    class="aside"
+    :class="{ 'translate-x-0': show, '-translate-x-full': !show }"
+    w="~ full md:max" h="~ full md:100dvh" p3 pos="absolute md:fixed left-0" transform="~ translate md:translate-x-none" transition-all border="~ base none md:solid md:r-1" bg-inherit
+  >
     <nav class="nav" flex="~ col justify-start items-center" w-full h-full>
       <NuxtLink
-        v-for="link in links"
-        :key="link.slug"
-        :to="link.slug"
-        :class=" $route.name === link.slug.slice(1)
+        v-for="link in links" :key="link.slug" :to="link.slug"
+        :class="$route.name === link.slug.slice(1)
           ? 'bg-gray-500/30 color-white cursor-default'
           : 'hover:bg-gray-400:5'
-        "
-        flex="~ justify-start items-center"
-        w-full mb1 p3 rounded-lg
-        active:bg-gray-300:6
+        " flex="~ justify-start items-center" w-full mb1 p3 rounded-lg active:bg-gray-300:6
       >
         <Icon :name="link.icon" mr2 />
         <span capitalize>
